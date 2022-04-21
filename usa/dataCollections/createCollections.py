56d673bc441a0bc397ef45b8deb62766d3aaf48e
Sustain Account <sustain@cs.colostate.edu>
2022-04-20 21:48:15 -0600

    error check input

--- usa/dataCollections/createCollections.py
+++ usa/dataCollections/createCollections.py
@@ -5,7 +5,7 @@ import json, sys, pymongo, os, logging
 from time import sleep
 from os.path import exists
 from threading import Thread
-sys.path.insert(0, PATH_BASE+'/create')
+sys.path.insert(0, PATH_BASE)
 import utils
 
 pipeData = "water_quality_pipes.json"
@@ -18,8 +18,8 @@ PROGRESS_BASE = PATH_BASE+'/dataCollections/progressFiles'
 
 def createCollections(threadNumber, NUM_THREADS, batch):
 
-    site_line_matrix = os.path.expanduser(PATH_BASE+'/adjacentryMatrices/siteLineMatrix.json') # Move this files!
-    site_polygon_matrix = os.path.expanduser(PATH_BASE+'/adjacentryMatrices/sitePolygonMatrix.json') # Move this files!
+    site_line_matrix = os.path.expanduser(PATH_BASE+'/adjacentryMatrices/siteLineMatrix.json') # Move this file
+    site_polygon_matrix = os.path.expanduser(PATH_BASE+'/adjacentryMatrices/sitePolygonMatrix.json') # Move this file
     water_data_file_path = os.path.expanduser(PATH_BASE+'/download/data/combinedData/' + batch)
 
     errorFile = os.path.expanduser(PATH_BASE+'/dataCollections/error.log')
@@ -39,6 +39,7 @@ def createCollections(threadNumber, NUM_THREADS, batch):
     water_quality_data = utils.getJSON(water_data_file_path)
 
     documentsForThisThread = [water_quality_data[i] for i in range(len(water_quality_data)) if i == threadNumber or i%NUM_THREADS == threadNumber]
+
     documentsProcessedByThisThread = 0
     for document in documentsForThisThread:
         try:
@@ -85,7 +86,9 @@ def main(batch, NUM_THREADS):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 :
+    if len(sys.argv) != 3 :
+        print('Invalid Args')
         print(f'Usage: python3 {sys.argv[0]} <source_file_name> <number_of_threads>')
         print(f'ex: python3 {sys.argv[0]} cBatch1.json 6')
-    main(sys.argv[1], int(sys.argv[1]))
+    else:
+        main(sys.argv[1], int(sys.argv[2]))
